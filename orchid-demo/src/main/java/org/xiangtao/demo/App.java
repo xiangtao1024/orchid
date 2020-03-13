383be37d85047921fa6b686975962a039c001ab5
1782227898 <1782227898@qq.com>
2020-03-13 15:16:17 +0800

    项目结构搭建

---
+++ orchid-demo/src/main/java/org/xiangtao/demo/App.java
@@ -0,0 +1,9 @@
+package org.xiangtao.demo;
+
+import org.xiangtao.orchid.Orchid;
+
+public class App {
+	public static void main(String[] args) {
+		Orchid.start(App.class);
+	}
+}
---
+++ orchid-frame/src/main/java/org/xiangtao/orchid/Orchid.java
@@ -0,0 +1,11 @@
+package org.xiangtao.orchid;
+
+public class Orchid {
+	public static void start(Class<?> clz){
+		
+	}
+	
+	public static void stop(){
+		
+	}
+}
